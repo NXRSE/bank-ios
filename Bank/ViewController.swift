af34de16a082b49bb6557de5434e1650ce2806a1
Kyle Redelinghuys <kyle@ksred.me>
2015-11-26 20:19:22 -0500

    Initial Commit

---
+++ Bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Kyle Redelinghuys on 2015/11/26.
+//  Copyright © 2015 Kyle Redelinghuys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
